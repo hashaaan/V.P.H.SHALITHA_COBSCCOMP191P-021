bb2eaee882b7dbca49c8da177bbce9686f07cc12
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-09-13 14:01:57 +0530

    First commit

---
+++ V.P.H.SHALITHA_COBSCCOMP191P-021Tests/V_P_H_SHALITHA_COBSCCOMP191P_021Tests.swift
@@ -0,0 +1,34 @@
+//
+//  V_P_H_SHALITHA_COBSCCOMP191P_021Tests.swift
+//  V.P.H.SHALITHA_COBSCCOMP191P-021Tests
+//
+//  Created by HASHAN on 9/13/20.
+//  Copyright © 2020 NIBM-COBSCCOMP191P-021. All rights reserved.
+//
+
+import XCTest
+@testable import V_P_H_SHALITHA_COBSCCOMP191P_021
+
+class V_P_H_SHALITHA_COBSCCOMP191P_021Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
