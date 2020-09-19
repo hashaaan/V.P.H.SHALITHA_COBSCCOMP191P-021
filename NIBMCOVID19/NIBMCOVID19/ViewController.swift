ff2522e601abbd2f559fe6541ae14480fce49397
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-09-19 21:18:50 +0530

    Re add pods

---
+++ NIBMCOVID19/NIBMCOVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBMCOVID19
+//
+//  Created by HASHAN on 9/19/20.
+//  Copyright © 2020 NIBM-COBSCCOMP191P-021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
