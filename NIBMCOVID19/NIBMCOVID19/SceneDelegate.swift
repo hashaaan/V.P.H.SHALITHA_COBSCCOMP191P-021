e27b75bebf5c4f36d119cc05c4b56d467f05c766
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-09-19 22:21:05 +0530

    Re config firebase

--- NIBMCOVID19/NIBMCOVID19/SceneDelegate.swift
+++ NIBMCOVID19/NIBMCOVID19/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -14,10 +15,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        FirebaseApp.configure()
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController() // Your initial view controller.
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
